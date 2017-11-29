6611ed957390329609dc695e419c0e80690ecf11
inker <anton.veselyev@gmail.com>
2017-11-29 16:21:20 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ process.env.TZ = 'Europe/Moscow'
 
 const http = require('http')
 const express = require('express')
+const compression = require('compression')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const chalk = require('chalk').default
@@ -10,18 +11,28 @@ const { port } = require('../config.json').server
 
 const logger = require('./middleware/logger')
 const permissions = require('./middleware/permissions')
+const router = require('./routes')
 
 const db = require('./db')
 
+const routes = {
+  auth: [
+    'login',
+    'logout',
+    'verify',
+  ],
+  phones: [
+    'add',
+    'check',
+    'remove',
+  ],
+}
+
 const app = express()
 
-app.set('views', `${__dirname}/views`)
-app.set('view cache', true)
-
-app.use(require('compression')()) // should be first
-
-app.use(bodyParser.urlencoded({ extended: false })) // to support URL-encoded bodies
-app.use(bodyParser.json()) // to support JSON-encoded bodies
+app.use(compression())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(cors())
 
 app.use(logger())
@@ -29,8 +40,7 @@ app.use(permissions({
   exemptServices: 'auth',
 }))
 
-app.use('/auth', require('./routes/auth'))
-app.use('/phones', require('./routes/phones'))
+app.use(router(routes))
 
 app.use(express.static(`${__dirname}/client`))
 
